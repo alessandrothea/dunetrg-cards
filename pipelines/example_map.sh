9f7faed710060bbe63844171036901125851d30c
Alessandro Thea <alessandro.thea@cern.ch>
2025-08-13 11:19:20 -0500

    Updated example pipeline

--- pipelines/example_map.sh
+++ pipelines/example_map.sh
@@ -2,17 +2,18 @@
 
 # Configuration
 
-sample="bkg_vd"
+sample="eminus"
 
 declare -A stages_configs
 
 stages_configs["gen"]="${HOME}/app/dune-trg-sandbox/fcl/vd/prodbackground_radiological_decay0_dunevd10kt_1x8x6_patched.fcl"
 stages_configs["g4"]="${HOME}/app/dune-trg-sandbox/fcl/vd/standard_g4_dunevd10kt_1x8x6_3view_30deg.fcl"
 stages_configs["detsim"]="${HOME}/app/dune-trg-sandbox/fcl/vd/detsim_dunevd10kt_1x8x6_3view_30deg_notpcsigproc.fcl"
-stages_configs["tpg"]="${HOME}/app/dune-trg-sandbox/fcl/vd/tpgtree_vd_ST_with_ides.fcl"
+stages_configs["tpg"]="${HOME}/app/dune-trg-sandbox/fcl/vd/trig_studies_tpg_st_rs_ars.fcl"
 
 stages=("gen" "g4" "detsim" "tpg")
 n_ev=3
+skip_stages=0
 
 #-------------------------------------
 # Don't touch beyond this point
@@ -22,7 +23,6 @@ for i in "${!stages[@]}"; do
     cd ${base_dir}
     stage=${stages[$i]}
     echo ">>> Stage: '$stage' <<< "
-
 
     if [[ $i -eq 0 ]]; then
         k=${n_ev}
@@ -35,6 +35,11 @@ for i in "${!stages[@]}"; do
     cfg_file=${stages_configs[$stage]}
     out_dir=${base_dir}/${stage} 
     out_file=${out_dir}/${stage}_${sample}.root
+
+    if [[ $i -lt $skip_stages ]]; then
+        echo "   <skipping>"
+        continue
+    fi
 
     mkdir -p ${out_dir}    
     cd ${out_dir}
